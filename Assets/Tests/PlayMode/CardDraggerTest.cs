15c99fae3ef8a6039e4e90f74f7f896a754bd48c
Thana Sithisombath <sithisombath@cpp.edu>
2023-08-20 20:11:20 -0700

    Added a new unittest for hovering

--- Assets/Tests/PlayMode/CardDraggerTest.cs
+++ Assets/Tests/PlayMode/CardDraggerTest.cs
@@ -5,13 +5,65 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
+public class TestSetup
+{
+    private Mock<IMouseWrapper> mock;
+
+    public TestSetup()
+    {
+        mock = new Mock<IMouseWrapper>();
+    }
+
+    public Mock<IMouseWrapper> Get()
+    {
+        return mock;
+    }
+
+    public TestSetup WithPlayerClicksOnHand()
+    {
+        mock.Setup(x => x.PlayerPressesLeftClick()).Returns(true);
+        mock.Setup(x => x.IsOnHand()).Returns(true);
+        return this;
+    }
+
+    public TestSetup WithPlayerDoesNotClickOnHand()
+    {
+        mock.Setup(x => x.PlayerPressesLeftClick()).Returns(false);
+        return this;
+    }
+
+    public TestSetup WithPickedUpCard(Card card)
+    {
+        mock.Setup(x => x.GetHitComponent<Card>()).Returns(card);
+        WithNoHover();
+        return this;
+    }
+
+    private TestSetup WithNoHover()
+    {
+        mock.Setup(x => x.IsOnHand()).Returns(true);
+        return this;
+    }
+
+    public TestSetup WithHover(Card card)
+    {
+        mock.Setup(x => x.IsOnHand()).Returns(true);
+        mock.Setup(x => x.GetHitComponent<Card>()).Returns(card);
+        return this;
+    }
+
+    public TestSetup WithAddCardToSelection()
+    {
+        mock.Setup(x => x.PlayerReleasesLeftClick()).Returns(true);
+        mock.Setup(x => x.IsOnSelection()).Returns(true);
+        return this;
+    }
+}
+
 public class CardDraggerTest : MonoBehaviour
 {
     private Hand hand;
-    private GainActionManager gainActions;
-    private RerollManager reroll;
     private SelectionScreen selectionScreen;
-    private HoverAnimation hoverAnimation;
 
     [SetUp]
     public void Setup()
@@ -19,31 +71,21 @@ public class CardDraggerTest : MonoBehaviour
         var gameObject = new GameObject();
         hand = gameObject.AddComponent<Hand>();
         selectionScreen = gameObject.AddComponent<SelectionScreen>();
-        hoverAnimation = gameObject.AddComponent<HoverAnimation>();
     }
 
     [TearDown]
     public void TearDown()
     {
         FindObjectsOfType<Hand>().ToList().ForEach(o => DestroyImmediate(o.gameObject));
-        FindObjectsOfType<GainActionManager>().ToList().ForEach(o => DestroyImmediate(o.gameObject));
-        FindObjectsOfType<RerollManager>().ToList().ForEach(o => DestroyImmediate(o.gameObject));
         FindObjectsOfType<SelectionScreen>().ToList().ForEach(o => DestroyImmediate(o.gameObject));
-        FindObjectsOfType<HoverAnimation>().ToList().ForEach(o => DestroyImmediate(o.gameObject));
     }
 
     [UnityTest]
-    public IEnumerator XXX()
+    public IEnumerator GivenClicksOnHandPickACardAndAddToSelection_Update_ExpectNoErrors()
     {
         // Arrange
-        var mouse = new Mock<IMouseWrapper>();
         var card = new GameObject().AddComponent<Card>();
-        mouse.Setup(x => x.PlayerPressesLeftClick()).Returns(true);
-        mouse.Setup(x => x.IsOnHand()).Returns(true);
-        mouse.Setup(x => x.GetHitComponent<Card>()).Returns(card);
-        mouse.Setup(x => x.IsOnHand()).Returns(true);
-        mouse.Setup(x => x.PlayerReleasesLeftClick()).Returns(true);
-        mouse.Setup(x => x.IsOnSelection()).Returns(true);
+        var mouse = new TestSetup().WithPlayerClicksOnHand().WithPickedUpCard(card).WithAddCardToSelection().Get();
         var cardDragger = new GameObject().AddComponent<CardDragger>();
         cardDragger.SetMouseWrapper(mouse.Object);
         yield return new WaitForEndOfFrame();
@@ -54,4 +96,21 @@ public class CardDraggerTest : MonoBehaviour
         Assert.DoesNotThrow(() => cardDragger.UpdateLoop());
     }
 
+    [UnityTest]
+    public IEnumerator GivenNoSelectedCards_Update_ExpectNoErrors()
+    {
+        // Arrange
+        var card = new GameObject().AddComponent<Card>();
+        var mouse = new TestSetup().WithPlayerDoesNotClickOnHand().WithHover(card).Get();
+        var gameObject = new GameObject();
+        gameObject.AddComponent<HoverAnimation>();
+        var cardDragger = gameObject.AddComponent<CardDragger>();
+        cardDragger.SetMouseWrapper(mouse.Object);
+        yield return new WaitForEndOfFrame();
+
+        // Act
+
+        // Assert
+        Assert.DoesNotThrow(() => cardDragger.UpdateLoop());
+    }
 }
